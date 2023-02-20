1e91438f2d81d385cb1481d3eed8be690430767d
Lorenz <lorenzbuser@gmail.com>
2023-02-19 21:05:05 -0500

    Wrote script to read data from transactions table in Azolla backend into a Pandas DataFrame

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
-print('Hello world! \n')
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 
-print('Adapted version in master')
+db_host = "gupeveno.mysql.db.hostpoint.ch"
+db_name = "gupeveno_backendAzolla"
+db_username = "gupeveno_azbeu"
+db_password = "backend_GoNet0"
+
+db_connection_string = f"mysql+pymysql://{db_username}:{db_password}@{db_host}/{db_name}"
+
+db_engine = create_engine(db_connection_string)
+db_connection = db_engine.connect()
+
+query = text("SELECT * FROM transactions")
+
+df = pd.read_sql(query, con=db_connection)
+
+print(df)
